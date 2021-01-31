1172d9fbc1f5b8a17490059a3ea77799b2aa410c
Nikolas797 <nikolas797@mail.ru>
2021-01-31 14:53:17 +0300

    ДЗ#3 - тесты для проверки точек

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.sandbox;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTests {
+
+    @Test
+    // тест на две точки с разными координатами, включая отрицательные значения
+    public void test1(){
+        Point p1 = new Point(-1,4);
+        Point p2 = new Point(-2,-3);
+        Assert.assertEquals(Math.round(p1.distance(p2)), 7);
+    }
+
+    @Test
+    // тест для двух точек с разным координатами
+    public void test2(){
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(1,1);
+        Assert.assertEquals(Math.round(p1.distance(p2)), 1);
+    }
+
+    @Test
+    //тест на две точки с равными координатами
+    public void test3() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(0, 0);
+        Assert.assertEquals(Math.round(p1.distance(p2)), 0);
+    }
+}
