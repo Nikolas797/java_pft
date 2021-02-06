2a57545ea193a1337a56db240f8b8a3e9fc9b4cc
Nikolas797 <nikolas797@mail.ru>
2021-02-06 16:14:17 +0300

    Создание общего класса для тестов + удаление теста на создание новго контакта внутри класса Group

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -8,154 +8,18 @@ import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 
-public class GroupCreationTests {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+public class GroupCreationTests extends TestBase {
 
-  @BeforeClass(alwaysRun = true)
-  public void setUp() throws Exception {
-    System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-    driver = new ChromeDriver();
-    baseUrl = "https://www.google.com/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    driver.get("http://localhost/addressbook/group.php");
-    login("admin", "secret");
-
-  }
-
-  private void login(String username, String password) {
-    submitGroupCreation("user");
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys(username);
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys(password);
-    driver.findElement(By.id("LoginForm")).submit();
-  }
 
   @Test
   public void testGroupCreation() throws Exception {
     gotoGroupPage("groups");
-    initGroupCreation("new");
-    initGroupCreation("group_name");
+    submitGroupCreation("new");
+    submitGroupCreation("group_name");
     fillGroupForm(new GroupData("test2", "test3", "test4"));
     submitGroupCreation("submit");
     returnToGroupPage("group page");
-    exitLogout("Logout");
+    gotoGroupPage("Logout");
   }
 
-  private void exitLogout(String logout) {
-    driver.findElement(By.linkText(logout)).click();
-  }
-
-  private void returnToGroupPage(String s) {
-    exitLogout(s);
-  }
-
-  private void submitGroupCreation(String submit) {
-    initGroupCreation(submit);
-  }
-
-  private void fillGroupForm(GroupData groupData) {
-    driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  private void initGroupCreation(String s) {
-    driver.findElement(By.name(s)).click();
-  }
-
-  private void gotoGroupPage(String groups) {
-    exitLogout(groups);
-  }
-
-  @AfterClass(alwaysRun = true)
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
-
-
-  @Test
-  public void testNewContact() throws Exception {
-    login("admin", "secret");
-    addNewContact("add new");
-    fillNewContact(new ContactData("Nikolai", "Kolesnikov", "Koleso", "QA", "AH", "8-985-759-2332", "nikolas797@mail.ru", "7", "December", "1997"));
-  }
-
-  private void fillNewContact(ContactData contactData) {
-    driver.findElement(By.name("firstname")).click();
-    driver.findElement(By.name("firstname")).clear();
-    driver.findElement(By.name("firstname")).sendKeys(contactData.getName());
-    driver.findElement(By.name("lastname")).clear();
-    driver.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-    driver.findElement(By.name("nickname")).clear();
-    driver.findElement(By.name("nickname")).sendKeys(contactData.getNik());
-    driver.findElement(By.name("title")).click();
-    driver.findElement(By.name("title")).clear();
-    driver.findElement(By.name("title")).sendKeys(contactData.getTitle());
-    driver.findElement(By.name("company")).click();
-    driver.findElement(By.name("company")).clear();
-    driver.findElement(By.name("company")).sendKeys(contactData.getCompany());
-    driver.findElement(By.name("mobile")).click();
-    driver.findElement(By.name("mobile")).clear();
-    driver.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
-    driver.findElement(By.name("email")).click();
-    driver.findElement(By.name("email")).clear();
-    driver.findElement(By.name("email")).sendKeys(contactData.getMail());
-    driver.findElement(By.name("bday")).click();
-    new Select(driver.findElement(By.name("bday"))).selectByVisibleText(contactData.getBday());
-    driver.findElement(By.name("bmonth")).click();
-    new Select(driver.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBmonth());
-    driver.findElement(By.name("byear")).click();
-    driver.findElement(By.name("byear")).clear();
-    driver.findElement(By.name("byear")).sendKeys(contactData.getByear());
-    driver.findElement(By.name("new_group")).click();
-    driver.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-  }
-
-  private void addNewContact(String s) {
-    driver.findElement(By.linkText(s)).click();
-  }
 }
