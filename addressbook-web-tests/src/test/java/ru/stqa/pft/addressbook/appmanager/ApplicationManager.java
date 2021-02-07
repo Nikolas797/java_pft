68419d92e58b6423d3d21f374fc4b381fd5c52be
Nikolas797 <nikolas797@mail.ru>
2021-02-07 19:07:24 +0300

    Создание двухуровненвой архитектуры

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -0,0 +1,112 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import ru.stqa.pft.addressbook.GroupData;
+import java.util.concurrent.TimeUnit;
+import static org.testng.Assert.fail;
+
+public class ApplicationManager {
+    private WebDriver wd;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    public void init() {
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        wd = new ChromeDriver();
+        baseUrl = "https://www.google.com/";
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/group.php");
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+      wd.findElement(By.name("user")).clear();
+      wd.findElement(By.name("user")).sendKeys(username);
+      wd.findElement(By.name("pass")).clear();
+      wd.findElement(By.name("pass")).sendKeys(password);
+      wd.findElement(By.id("LoginForm")).submit();
+    }
+
+    public void exitLogout() {
+      wd.findElement(By.linkText("Logout")).click();
+    }
+
+    public void returnToGroupPage() {
+        wd.findElement(By.linkText("group page")).click();
+    }
+
+    public void submitGroupCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    public void fillGroupForm(GroupData groupData) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    public void initGroupCreation() {
+        wd.findElement(By.name("new")).click();
+    }
+
+    public void gotoGroupPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+
+    public void stop() {
+        wd.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+          fail(verificationErrorString);
+        }
+    }
+
+    public boolean isElementPresent(By by) {
+      try {
+        wd.findElement(by);
+        return true;
+      } catch (NoSuchElementException e) {
+        return false;
+      }
+    }
+
+    public boolean isAlertPresent() {
+      try {
+        wd.switchTo().alert();
+        return true;
+      } catch (NoAlertPresentException e) {
+        return false;
+      }
+    }
+
+    public String closeAlertAndGetItsText() {
+      try {
+        Alert alert = wd.switchTo().alert();
+        String alertText = alert.getText();
+        if (acceptNextAlert) {
+          alert.accept();
+        } else {
+          alert.dismiss();
+        }
+        return alertText;
+      } finally {
+        acceptNextAlert = true;
+      }
+    }
+
+    public void deleteSelectedGroups() {
+      wd.findElement(By.name("delete")).click();
+    }
+
+    public void selectGroup() {
+      wd.findElement(By.name("selected[]")).click();
+    }
+}
