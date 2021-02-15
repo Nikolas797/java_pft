d5158cfa7abe9c197277631d651b4357a2705a1d
Nikolas797 <nikolas797@mail.ru>
2021-02-15 19:06:12 +0300

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -12,10 +12,18 @@ public class NavigationHelper extends HelperBase {
     public void goToAddNew() { click(By.linkText("add new")); }
 
     public void gotoGroupPage() {
+        if(isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))){
+            return;
+        }
         click(By.linkText("groups"));
     }
 
     public void goToHomeContact() {
+        if (isElementPresent(By.id("maintable"))){
+            return;
+        }
         click(By.linkText("home"));
     }
 
