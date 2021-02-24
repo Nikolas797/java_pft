1970dea8a128342842082880d95a286ae846dad6
Nikolas797 <nikolas797@mail.ru>
2021-02-25 01:18:12 +0300

    Лямбда выражения для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddNewContact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddNewContact.java
@@ -1,9 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,19 +13,19 @@ public class AddNewContact extends TestBase {
     app.getNavigationHelper().goToHomeContact();
     List<ContactData> before = app.getContactHelper().getContactList();
     app.getNavigationHelper().goToAddNew();
-    ContactData contact = new ContactData("nk", "emp", "qa", "AH");
+    ContactData contact = new ContactData("QQQ", "emp", "qa", "AH");
     app.getContactHelper().createContact(contact);
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
+//    int max = 0;
+//    for(ContactData c : after){
+//      if(c.getId() > max) {
+//        max = c.getId();
+//      }
+//    }
 
-    int max = 0;
-    for(ContactData c : after){
-      if(c.getId() > max) {
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
