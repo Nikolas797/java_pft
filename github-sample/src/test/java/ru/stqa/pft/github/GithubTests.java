3fa9ffc45dfec7be886f3675ae9206ab32d7385e
Nikolas797 <nikolas797@mail.ru>
2021-04-04 18:40:47 +0300

    9.6 - REST: взаимодействие с GitHub

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub("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");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("Nikolas797", "java_pft")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+}
